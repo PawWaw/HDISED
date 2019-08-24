0d3980208aff8cc221674c68d38b4c269da31a2f
Paweł Wawszczyk <pawel.w@hotmail.com>
2019-08-24 13:29:07 +0200

    Data Generator

---
+++ DataGenerator/DataGenerator/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace DataGenerator
+{
+    partial class DataGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberCombobox = new System.Windows.Forms.ComboBox();
+            this.ColumnNumberLabel = new System.Windows.Forms.Label();
+            this.TypeFourLabel = new System.Windows.Forms.Label();
+            this.TypeCombobox4 = new System.Windows.Forms.ComboBox();
+            this.TypeThreeLabel = new System.Windows.Forms.Label();
+            this.TypeCombobox3 = new System.Windows.Forms.ComboBox();
+            this.TypeTwoLabel = new System.Windows.Forms.Label();
+            this.TypeCombobox2 = new System.Windows.Forms.ComboBox();
+            this.TypeOneLabel = new System.Windows.Forms.Label();
+            this.TypeCombobox1 = new System.Windows.Forms.ComboBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.TypeFiveLabel = new System.Windows.Forms.Label();
+            this.TypeCombobox5 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ElementsTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NumberCombobox
+            // 
+            this.NumberCombobox.FormattingEnabled = true;
+            this.NumberCombobox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.NumberCombobox.Location = new System.Drawing.Point(242, 50);
+            this.NumberCombobox.Name = "NumberCombobox";
+            this.NumberCombobox.Size = new System.Drawing.Size(121, 24);
+            this.NumberCombobox.TabIndex = 0;
+            this.NumberCombobox.SelectedIndexChanged += new System.EventHandler(this.NumberCombobox_SelectedIndexChanged);
+            // 
+            // ColumnNumberLabel
+            // 
+            this.ColumnNumberLabel.AutoSize = true;
+            this.ColumnNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ColumnNumberLabel.Location = new System.Drawing.Point(40, 50);
+            this.ColumnNumberLabel.Name = "ColumnNumberLabel";
+            this.ColumnNumberLabel.Size = new System.Drawing.Size(186, 25);
+            this.ColumnNumberLabel.TabIndex = 1;
+            this.ColumnNumberLabel.Text = "Number of columns:";
+            // 
+            // TypeFourLabel
+            // 
+            this.TypeFourLabel.AutoSize = true;
+            this.TypeFourLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TypeFourLabel.Location = new System.Drawing.Point(40, 290);
+            this.TypeFourLabel.Name = "TypeFourLabel";
+            this.TypeFourLabel.Size = new System.Drawing.Size(152, 25);
+            this.TypeFourLabel.TabIndex = 3;
+            this.TypeFourLabel.Text = "Type of column:";
+            // 
+            // TypeCombobox4
+            // 
+            this.TypeCombobox4.Enabled = false;
+            this.TypeCombobox4.FormattingEnabled = true;
+            this.TypeCombobox4.Items.AddRange(new object[] {
+            "Integer",
+            "Char",
+            "String",
+            "Float"});
+            this.TypeCombobox4.Location = new System.Drawing.Point(242, 290);
+            this.TypeCombobox4.Name = "TypeCombobox4";
+            this.TypeCombobox4.Size = new System.Drawing.Size(121, 24);
+            this.TypeCombobox4.TabIndex = 2;
+            // 
+            // TypeThreeLabel
+            // 
+            this.TypeThreeLabel.AutoSize = true;
+            this.TypeThreeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TypeThreeLabel.Location = new System.Drawing.Point(40, 230);
+            this.TypeThreeLabel.Name = "TypeThreeLabel";
+            this.TypeThreeLabel.Size = new System.Drawing.Size(152, 25);
+            this.TypeThreeLabel.TabIndex = 5;
+            this.TypeThreeLabel.Text = "Type of column:";
+            // 
+            // TypeCombobox3
+            // 
+            this.TypeCombobox3.Enabled = false;
+            this.TypeCombobox3.FormattingEnabled = true;
+            this.TypeCombobox3.Items.AddRange(new object[] {
+            "Integer",
+            "Char",
+            "String",
+            "Float"});
+            this.TypeCombobox3.Location = new System.Drawing.Point(242, 230);
+            this.TypeCombobox3.Name = "TypeCombobox3";
+            this.TypeCombobox3.Size = new System.Drawing.Size(121, 24);
+            this.TypeCombobox3.TabIndex = 4;
+            // 
+            // TypeTwoLabel
+            // 
+            this.TypeTwoLabel.AutoSize = true;
+            this.TypeTwoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TypeTwoLabel.Location = new System.Drawing.Point(40, 170);
+            this.TypeTwoLabel.Name = "TypeTwoLabel";
+            this.TypeTwoLabel.Size = new System.Drawing.Size(152, 25);
+            this.TypeTwoLabel.TabIndex = 7;
+            this.TypeTwoLabel.Text = "Type of column:";
+            // 
+            // TypeCombobox2
+            // 
+            this.TypeCombobox2.Enabled = false;
+            this.TypeCombobox2.FormattingEnabled = true;
+            this.TypeCombobox2.Items.AddRange(new object[] {
+            "Integer",
+            "Char",
+            "String",
+            "Float"});
+            this.TypeCombobox2.Location = new System.Drawing.Point(242, 170);
+            this.TypeCombobox2.Name = "TypeCombobox2";
+            this.TypeCombobox2.Size = new System.Drawing.Size(121, 24);
+            this.TypeCombobox2.TabIndex = 6;
+            // 
+            // TypeOneLabel
+            // 
+            this.TypeOneLabel.AutoSize = true;
+            this.TypeOneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TypeOneLabel.Location = new System.Drawing.Point(40, 110);
+            this.TypeOneLabel.Name = "TypeOneLabel";
+            this.TypeOneLabel.Size = new System.Drawing.Size(152, 25);
+            this.TypeOneLabel.TabIndex = 9;
+            this.TypeOneLabel.Text = "Type of column:";
+            // 
+            // TypeCombobox1
+            // 
+            this.TypeCombobox1.Enabled = false;
+            this.TypeCombobox1.FormattingEnabled = true;
+            this.TypeCombobox1.Items.AddRange(new object[] {
+            "Integer",
+            "Char",
+            "String",
+            "Float"});
+            this.TypeCombobox1.Location = new System.Drawing.Point(242, 110);
+            this.TypeCombobox1.Name = "TypeCombobox1";
+            this.TypeCombobox1.Size = new System.Drawing.Size(121, 24);
+            this.TypeCombobox1.TabIndex = 8;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GenerateButton.Location = new System.Drawing.Point(0, 473);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(398, 58);
+            this.GenerateButton.TabIndex = 10;
+            this.GenerateButton.Text = "GENERATE!";
+            this.GenerateButton.UseMnemonic = false;
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // TypeFiveLabel
+            // 
+            this.TypeFiveLabel.AutoSize = true;
+            this.TypeFiveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TypeFiveLabel.Location = new System.Drawing.Point(40, 350);
+            this.TypeFiveLabel.Name = "TypeFiveLabel";
+            this.TypeFiveLabel.Size = new System.Drawing.Size(152, 25);
+            this.TypeFiveLabel.TabIndex = 11;
+            this.TypeFiveLabel.Text = "Type of column:";
+            // 
+            // TypeCombobox5
+            // 
+            this.TypeCombobox5.Enabled = false;
+            this.TypeCombobox5.FormattingEnabled = true;
+            this.TypeCombobox5.Items.AddRange(new object[] {
+            "Integer",
+            "Char",
+            "String",
+            "Float"});
+            this.TypeCombobox5.Location = new System.Drawing.Point(242, 350);
+            this.TypeCombobox5.Name = "TypeCombobox5";
+            this.TypeCombobox5.Size = new System.Drawing.Size(121, 24);
+            this.TypeCombobox5.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(40, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Number of elements:";
+            // 
+            // ElementsTextbox
+            // 
+            this.ElementsTextbox.Location = new System.Drawing.Point(242, 410);
+            this.ElementsTextbox.Name = "ElementsTextbox";
+            this.ElementsTextbox.Size = new System.Drawing.Size(121, 22);
+            this.ElementsTextbox.TabIndex = 14;
+            // 
+            // DataGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(398, 531);
+            this.Controls.Add(this.ElementsTextbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TypeCombobox5);
+            this.Controls.Add(this.TypeFiveLabel);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.TypeOneLabel);
+            this.Controls.Add(this.TypeCombobox1);
+            this.Controls.Add(this.TypeTwoLabel);
+            this.Controls.Add(this.TypeCombobox2);
+            this.Controls.Add(this.TypeThreeLabel);
+            this.Controls.Add(this.TypeCombobox3);
+            this.Controls.Add(this.TypeFourLabel);
+            this.Controls.Add(this.TypeCombobox4);
+            this.Controls.Add(this.ColumnNumberLabel);
+            this.Controls.Add(this.NumberCombobox);
+            this.Name = "DataGenerator";
+            this.Text = "Data Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox NumberCombobox;
+        private System.Windows.Forms.Label ColumnNumberLabel;
+        private System.Windows.Forms.Label TypeFourLabel;
+        private System.Windows.Forms.ComboBox TypeCombobox4;
+        private System.Windows.Forms.Label TypeThreeLabel;
+        private System.Windows.Forms.ComboBox TypeCombobox3;
+        private System.Windows.Forms.Label TypeTwoLabel;
+        private System.Windows.Forms.ComboBox TypeCombobox2;
+        private System.Windows.Forms.Label TypeOneLabel;
+        private System.Windows.Forms.ComboBox TypeCombobox1;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Label TypeFiveLabel;
+        private System.Windows.Forms.ComboBox TypeCombobox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ElementsTextbox;
+    }
+}
+
---
+++ DataGenerator/DataGenerator/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGenerator
+{ 
+    public partial class DataGenerator : Form
+    {
+        private static int elements;
+        private static dynamic[] tabOne;
+        private static dynamic[] tabTwo;
+        private static dynamic[] tabThree;
+        private static dynamic[] tabFour;
+        private static dynamic[] tabFive;
+
+        public DataGenerator()
+        {
+            InitializeComponent();
+            NumberCombobox.SelectedIndex = 0;
+            TypeCombobox1.SelectedIndex = 0;
+            TypeCombobox2.SelectedIndex = 0;
+            TypeCombobox3.SelectedIndex = 0;
+            TypeCombobox4.SelectedIndex = 0;
+            TypeCombobox5.SelectedIndex = 0;
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if(ElementsTextbox.Text != "")
+            {
+                elements = Convert.ToInt32(ElementsTextbox.Text);
+                tabOne = new dynamic[elements];
+                tabTwo = new dynamic[elements];
+                tabThree = new dynamic[elements];
+                tabFour = new dynamic[elements];
+                tabFive = new dynamic[elements];
+                if (NumberCombobox.SelectedIndex == 0)
+                {
+                    Combobox1(sender, e);
+                    saveToFile(1);
+                }
+                else if (NumberCombobox.SelectedIndex == 1)
+                {
+                    Combobox1(sender, e);
+                    Combobox2(sender, e);
+                    saveToFile(2);
+                }
+                else if (NumberCombobox.SelectedIndex == 2)
+                {
+                    Combobox1(sender, e);
+                    Combobox2(sender, e);
+                    Combobox3(sender, e);
+                    saveToFile(3);
+                }
+                else if (NumberCombobox.SelectedIndex == 3)
+                {
+                    Combobox1(sender, e);
+                    Combobox2(sender, e);
+                    Combobox3(sender, e);
+                    Combobox4(sender, e);
+                    saveToFile(4);
+                }
+                else if (NumberCombobox.SelectedIndex == 4)
+                {
+                    Combobox1(sender, e);
+                    Combobox2(sender, e);
+                    Combobox3(sender, e);
+                    Combobox4(sender, e);
+                    Combobox5(sender, e);
+                    saveToFile(5);
+                }
+            }
+        }
+
+        private void Combobox1(object sender, EventArgs e)
+        {
+            if (TypeCombobox1.SelectedItem.ToString().Equals("Integer"))
+            {
+                tabOne = GenIntTab(elements);
+            }
+            else if (TypeCombobox1.SelectedItem.ToString().Equals("Float"))
+            {
+                tabOne = GenFloatTab(elements);
+            }
+            else if (TypeCombobox1.SelectedItem.ToString().Equals("Char"))
+            {
+                tabOne = GenCharTab(elements);
+            }
+            else if (TypeCombobox1.SelectedItem.ToString().Equals("String"))
+            {
+                tabOne = GenStringTab(elements);
+            }
+        }
+
+        private void Combobox2(object sender, EventArgs e)
+        {
+            if (TypeCombobox2.SelectedItem.ToString().Equals("Integer"))
+            {
+                tabTwo = GenIntTab(elements);
+            }
+            else if (TypeCombobox2.SelectedItem.ToString().Equals("Float"))
+            {
+                tabTwo = GenFloatTab(elements);
+            }
+            else if (TypeCombobox2.SelectedItem.ToString().Equals("Char"))
+            {
+                tabTwo = GenCharTab(elements);
+            }
+            else if (TypeCombobox2.SelectedItem.ToString().Equals("String"))
+            {
+                tabTwo = GenStringTab(elements);
+            }
+        }
+
+        private void Combobox3(object sender, EventArgs e)
+        {
+            if (TypeCombobox3.SelectedItem.ToString().Equals("Integer"))
+            {
+                tabThree = GenIntTab(elements);
+            }
+            else if (TypeCombobox3.SelectedItem.ToString().Equals("Float"))
+            {
+                tabThree = GenFloatTab(elements);
+            }
+            else if (TypeCombobox3.SelectedItem.ToString().Equals("Char"))
+            {
+                tabThree = GenCharTab(elements);
+            }
+            else if (TypeCombobox3.SelectedItem.ToString().Equals("String"))
+            {
+                tabThree = GenStringTab(elements);
+            }
+        }
+
+        private void Combobox4(object sender, EventArgs e)
+        {
+            if (TypeCombobox4.SelectedItem.ToString().Equals("Integer"))
+            {
+                tabFour = GenIntTab(elements);
+            }
+            else if (TypeCombobox4.SelectedItem.ToString().Equals("Float"))
+            {
+                tabFour = GenFloatTab(elements);
+            }
+            else if (TypeCombobox4.SelectedItem.ToString().Equals("Char"))
+            {
+                tabFour = GenCharTab(elements);
+            }
+            else if (TypeCombobox4.SelectedItem.ToString().Equals("String"))
+            {
+                tabFour = GenStringTab(elements);
+            }
+        }
+
+        private void Combobox5(object sender, EventArgs e)
+        {
+            if (TypeCombobox5.SelectedItem.ToString().Equals("Integer"))
+            {
+                tabFive = GenIntTab(elements);
+            }
+            else if (TypeCombobox5.SelectedItem.ToString().Equals("Float"))
+            {
+                tabFive = GenFloatTab(elements);
+            }
+            else if (TypeCombobox5.SelectedItem.ToString().Equals("Char"))
+            {
+                tabFive = GenCharTab(elements);
+            }
+            else if (TypeCombobox5.SelectedItem.ToString().Equals("String"))
+            {
+                tabFive = GenStringTab(elements);
+            }
+        }
+
+        private dynamic[] GenIntTab(int elements)
+        {
+            dynamic[] tab = new dynamic[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                if(i != 0)
+                {
+                    do
+                    {
+                        Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                        tab[i] = random.Next(0, 100);
+                    } while (tab[i] == tab[i - 1]);
+                }
+                else
+                {
+                    Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                    tab[i] = random.Next(0, 100);
+                }
+            }
+            return tab;
+        }
+
+        private dynamic[] GenFloatTab(int elements)
+        {
+            dynamic[] tab = new dynamic[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                if (i != 0)
+                {
+                    do
+                    {
+                        Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                        tab[i] = random.NextDouble() * 100;
+                    } while (tab[i] == tab[i - 1]);
+                }
+                else
+                {
+                    Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                    tab[i] = random.NextDouble() * 100;
+                }
+            }
+            return tab;
+        }
+
+        private dynamic[] GenCharTab(int elements)
+        {
+            dynamic[] tab = new dynamic[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                if (i != 0)
+                {
+                    do
+                    {
+                        Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                        tab[i] = (char)random.Next('a', 'z');
+                    } while (tab[i] == tab[i - 1]);
+                }
+                else
+                {
+                    Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                    tab[i] = (char)random.Next('a', 'z');
+                }
+            }
+            return tab;
+        }
+
+        private dynamic[] GenStringTab(int elements)
+        {
+            int length = 10;
+            dynamic[] tab = new dynamic[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                if (i != 0)
+                {
+                    do
+                    {
+                        Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                        tab[i] = new string(Enumerable.Repeat(chars, length)
+                          .Select(s => s[random.Next(s.Length)]).ToArray());
+                    } while (tab[i] == tab[i - 1]);
+                }
+                else
+                {
+                    Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+                    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                    tab[i] = new string(Enumerable.Repeat(chars, length)
+                      .Select(s => s[random.Next(s.Length)]).ToArray());
+                }
+            }
+            return tab;
+        }
+
+        private void NumberCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(NumberCombobox.SelectedIndex == 0)
+            {
+                TypeCombobox1.Enabled = true;
+                TypeCombobox2.Enabled = false;
+                TypeCombobox3.Enabled = false;
+                TypeCombobox4.Enabled = false;
+                TypeCombobox5.Enabled = false;
+            }
+            else if(NumberCombobox.SelectedIndex == 1)
+            {
+                TypeCombobox1.Enabled = true;
+                TypeCombobox2.Enabled = true;
+                TypeCombobox3.Enabled = false;
+                TypeCombobox4.Enabled = false;
+                TypeCombobox5.Enabled = false;
+            }
+            else if (NumberCombobox.SelectedIndex == 2)
+            {
+                TypeCombobox1.Enabled = true;
+                TypeCombobox2.Enabled = true;
+                TypeCombobox3.Enabled = true;
+                TypeCombobox4.Enabled = false;
+                TypeCombobox5.Enabled = false;
+            }
+            else if (NumberCombobox.SelectedIndex == 3)
+            {
+                TypeCombobox1.Enabled = true;
+                TypeCombobox2.Enabled = true;
+                TypeCombobox3.Enabled = true;
+                TypeCombobox4.Enabled = true;
+                TypeCombobox5.Enabled = false;
+            }
+            else 
+            {
+                TypeCombobox1.Enabled = true;
+                TypeCombobox2.Enabled = true;
+                TypeCombobox3.Enabled = true;
+                TypeCombobox4.Enabled = true;
+                TypeCombobox5.Enabled = true;
+            }
+        }
+
+        private void saveToFile(int columns)
+        {
+            string path = @"C:\Users\pawel\Desktop\MyTest.txt";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            FileStream fs = File.Create(path);
+
+            fs.Close();
+
+            var txt = new StringBuilder();
+
+            for (int i = 0; i < elements; i++)
+            {
+                if(columns == 1)
+                {
+                    var newLine = string.Format(tabOne[i]);
+                    txt.AppendLine(newLine);
+                }
+                else if(columns == 2)
+                {
+                    var newLine = string.Format(tabOne[i] + "," + tabTwo[i]);
+                    txt.AppendLine(newLine);
+                }
+                else if(columns == 3)
+                {
+                    var newLine = string.Format(tabOne[i] + "," + tabTwo[i] + "," + tabThree[i]);
+                    txt.AppendLine(newLine);
+                }
+                else if(columns == 4)
+                {
+                    var newLine = string.Format(tabOne[i] + "," + tabTwo[i] + "," + tabThree[i] + "," + tabFour[i]);
+                    txt.AppendLine(newLine);
+                }
+                else if(columns == 5)
+                {
+                    var newLine = string.Format(tabOne[i] + "," + tabTwo[i] + "," + tabThree[i] + "," + tabFour[i] + "," + tabFive[i]);
+                    txt.AppendLine(newLine);
+                }
+            }
+            File.WriteAllText(path, txt.ToString());
+        }
+    }
+}
